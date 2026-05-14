d33a8b7a8d8537699c0205d13d72689cda673e07
danyelvarejao <danyelvarejao@gmail.com>
2026-05-14 15:36:57 -0300

    update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,20 @@
+rm -rf ~/.zshrc
+rm -rf ~/.ideavimrc
+rm -rf ~/.vimrc
+rm -rf ~/.XCompose
+rm -rf ~/.config/alacritty
+rm -rf ~/.config/ashell
+rm -rf ~/.config/btop/btop.conf
+rm -rf ~/.config/git
+rm -rf ~/.config/hypr
+rm -rf ~/.config/lazygit
+rm -rf ~/.config/nvim
+rm -rf ~/.config/starship.toml
+rm -rf ~/.config/uwsm
+rm -rf ~/.config/walker
+rm -rf ~/.config/waybar
+rm -rf ~/.config/mimeapps.list
+
 stow alacritty
 stow ashell
 stow btop
@@ -8,7 +25,6 @@ stow lazygit
 stow nvim
 stow starship
 stow uwsm
-stow vscode
 stow walker
 stow waybar
 stow xcompose

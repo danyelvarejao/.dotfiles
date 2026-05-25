afdb2f8d8c9b4b3efcf7191f908e51487eb777f3
danyelvarejao <danyelvarejao@gmail.com>
2026-05-25 20:36:48 -0300

    feat: add neovide configs

---
+++ nvim/.config/nvim/lua/config/neovide.lua
@@ -0,0 +1,18 @@
+vim.g.neovide_scale_factor = 0.8
+vim.g.neovide_normal_opacity = 0.75
+vim.o.guifont = "JetBrainsMono Nerd Font:h14"
+
+vim.g.neovide_scroll_animation_length = 0
+vim.g.neovide_scroll_animation_far_lines = 5
+
+vim.g.neovide_cursor_animation_length = 0.15
+vim.g.neovide_cursor_trail_size = 0.9
+
+-- typing animation
+vim.g.neovide_cursor_short_animation_length = 0.25
+
+vim.g.neovide_hide_mouse_when_typing = true
+vim.g.neovide_cursor_unfocused_outline_width = 0.125
+
+vim.g.neovide_cursor_vfx_mode = { "railgun", "pixiedust" }
+vim.g.neovide_cursor_vfx_particle_lifetime = 0.7 -- railgun
